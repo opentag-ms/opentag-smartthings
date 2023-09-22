a4b89b819db4cdb1bfbd05dd5df9f085e3a11406
lbrentrup <lbrentrup@informationsfabrik.com>
2023-09-22 20:57:58 +0200

    Initial commit

---
+++ BLE_Schlüsselbund.ino
@@ -0,0 +1,46 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) { deviceConnected = true; };
+    void onDisconnect(BLEServer* pServer) { deviceConnected = false; }
+};
+
+void setup() {
+  BLEDevice::init("OpenSchlüsselbund");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+      if (deviceConnected) {
+          pCharacteristic->setValue("Connected");
+          pCharacteristic->notify();
+          delay(1000);
+      }
+}
---
+++ reciever.py
@@ -0,0 +1,29 @@
+import asyncio
+from bleak import BleakClient, BleakScanner
+
+CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+
+
+async def find_device(name: str):
+    devices = await BleakScanner.discover()
+    for device in devices:
+        if device.name == name:
+            print()
+            print(f"Name: {device.name}")
+            print(f"Address: {device.address}")
+            print(f"Details: {device.details}")
+            print(f"Metadata: {device.metadata}")
+            print(f"RSSI: {device.rssi}")
+            return device.address
+
+mac_address = asyncio.run(find_device("OpenSchlüsselbund"))
+
+
+async def run(address):
+    async with BleakClient(address) as client:
+        while True:
+            value = await client.read_gatt_char(CHARACTERISTIC_UUID)
+            print(value.decode())
+
+asyncio.run(run(mac_address))  # Replace with your ESP32's MAC address
